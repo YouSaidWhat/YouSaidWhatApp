9806893de59979732514fd0bae589ac601880559
slang21 <slang4201jr@gmail.com>
2023-10-31 19:09:55 -0700

    add contact logic

--- App.tsx
+++ App.tsx
@@ -2,23 +2,56 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './Screens/Home';
 import SettingsScreen from './Screens/Settings';
+import { PaperProvider } from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { NewQuote } from './Screens/NewQuote';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+const HomeNavigation = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="AddQuote" component={NewQuote} options={{
+        headerTitle: "Add Quote",
+      }}/>
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="MainExperience" component={HomeNavigation} options={{
+          headerShown: false,
+          tabBarLabel: 'Feed',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="home" color={color} size={size} />
+          ),
+        }}/>
+        <Tab.Screen name="Settings" component={SettingsScreen} options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="account" color={color} size={size} />
+          ),
+        }}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-export default App;
+export default function Main() {
+  return (
+    <PaperProvider>
+      <App />
+    </PaperProvider>
+  );
+}
 
 const styles = StyleSheet.create({
   container: {
--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { GuessingCard } from '../Components/GuessingCard';
+import { FAB } from 'react-native-paper';
+import { StyleSheet } from 'react-native';
+  
+const styles = StyleSheet.create({
+    fab: {
+      position: 'absolute',
+      margin: 16,
+      right: 0,
+      bottom: 0,
+    }
+});
+  
 
-
-const HomeScreen = () => {
+const HomeScreen = ({navigation}) => {
     return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home!</Text>
-        </View>
+        <>
+            <GuessingCard userName="John" quote="I'm a quote" options={["John", "Paul", "George", "Ringo"]} correctOption="John"/>
+            <FAB
+                icon="plus"
+                style={styles.fab}
+                onPress={() => navigation.navigate('AddQuote')}
+            />
+        </>
     );
 }
 
